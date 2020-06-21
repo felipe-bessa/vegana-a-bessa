1ee9903817f630d2980998b2fc71bb2dc8e4c940
Felipe Bessa <felipe-bessa@outlook.com>
2020-06-20 23:16:28 -0300

    Incluindo cabeçalho responsivo na pagina de receitas

--- frontend/src/pages/Recipes/index.js
+++ frontend/src/pages/Recipes/index.js
@@ -1,15 +1,44 @@
 // pages/Recipes/index.js
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
 
 import './styles.css';
+
+import Home from '../Home';
+import About from '../About';
 
 import Footer from '../../components/Footer/Footer';
 
 export default function Recipes() {
   return(
     <div>
-      <h1>Recipes</h1>
+      <header>
+        <nav className="navBar">
+          <input type="checkbox" id="check"></input>
+          <label for="check" className="checkbtn">
+            <FaBars color="white" />
+          </label>
+          <label className="logo">Vegana à Bessa</label>
+          <ul>
+            <li>
+              <Link to={Home}>Home</Link>
+            </li>
+            <li>
+              <Link to={About}>Sobre</Link>
+            </li>
+            <li>
+              <Link to="#">Receitas</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      
+      <div>
+        
+      </div>
+      
       <Footer />
     </div>
   );
